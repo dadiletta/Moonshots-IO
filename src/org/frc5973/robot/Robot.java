67089042662c1e065ebe21db2cab751d5697d6ba
dadiletta <dan.adiletta@gmail.com>
2017-02-04 12:54:04 -0500

    Init

---
+++ src/org/frc5973/robot/Robot.java
@@ -0,0 +1,30 @@
+/* Created Tue Jan 24 09:27:35 EST 2017 */
+package org.frc5973.robot;
+
+import org.strongback.Strongback;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+    @Override
+    public void robotInit() {
+    }
+
+    @Override
+    public void teleopInit() {
+        // Start Strongback functions ...
+        Strongback.start();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    @Override
+    public void disabledInit() {
+        // Tell Strongback that the robot is disabled so it can flush and kill commands.
+        Strongback.disable();
+    }
+
+}
---
+++ testsrc/org/frc5973/robot/TestRobot.java
@@ -0,0 +1,13 @@
+/* Created Tue Jan 24 09:27:35 EST 2017 */
+package org.frc5973.robot;
+
+import org.junit.Test;
+
+public class TestRobot {
+
+    @Test
+    public void test() {
+        // do something here
+    }
+
+}
